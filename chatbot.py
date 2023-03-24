be82c6a3edc06813b77a860d4023f77723ec98be
Youngmin Kim <aldente0630@gmail.com>
2023-03-24 22:04:53 +0900

    make slack-chatbot ver 0.1

---
+++ chatbot.py
@@ -0,0 +1,97 @@
+import os
+from typing import Optional
+import yaml
+import openai
+
+
+class ChatBot:
+    def __init__(
+        self,
+        api_secret_key: str,
+        model: str,
+        temperature: float,
+        max_tokens: int,
+        top_p: int,
+        frequency_penalty: int,
+        presence_penalty: int,
+    ):
+        openai.api_key = api_secret_key
+        self.model = model
+        self.temperature = temperature
+        self.max_tokens = max_tokens
+        self.top_p = top_p
+        self.frequency_penalty = frequency_penalty
+        self.presence_penalty = presence_penalty
+
+    def create(
+        self,
+        user_content: str,
+        system_content: Optional[str] = None,
+        assistant_content: Optional[str] = None,
+    ) -> str:
+        if self.model == "text-davinchi-003":
+            completion = openai.Completion.create(
+                model=self.model,
+                prompt=user_content,
+                temperature=self.temperature,
+                max_tokens=self.max_tokens,
+                top_p=self.top_p,
+                frequency_penalty=self.frequency_penalty,
+                presence_penalty=self.presence_penalty,
+            )
+            return completion["choices"][0]["text"]
+
+        if self.model == "gpt-3.5-turbo":
+            messages = [{"role": "user", "content": user_content}]
+            if system_content is not None:
+                messages.append({"role": "system", "content": system_content})
+            if assistant_content is not None:
+                messages.append({"role": "assistant", "content": assistant_content})
+
+            print(messages)
+            completion = openai.ChatCompletion.create(
+                model=self.model,
+                messages=messages,
+                temperature=self.temperature,
+                max_tokens=self.max_tokens,
+                top_p=self.top_p,
+                frequency_penalty=self.frequency_penalty,
+                presence_penalty=self.presence_penalty,
+            )
+            return completion["choices"][0]["message"]["content"]
+
+        msg = f"The model, {self.model} is not supported."
+        raise ValueError(msg)
+
+    def answer_directly(self) -> None:
+        prompt = input("Enter the prompt: ")
+        response = self.create(prompt)
+        print(response.strip())
+
+
+if __name__ == "__main__":
+    config_path = os.path.join("config.yaml")
+
+    with open(config_path, encoding="utf-8") as file:
+        config = yaml.safe_load(file)
+
+    api_secret_key = config["credentials"]["openai_api_secret_key"]
+    model = config["chatbot"]["model"]
+    temperature = config["chatbot"]["temperature"]
+    max_tokens = config["chatbot"]["max_tokens"]
+    top_p = config["chatbot"]["top_p"]
+    frequency_penalty = config["chatbot"]["frequency_penalty"]
+    presence_penalty = config["chatbot"]["presence_penalty"]
+
+    chatbot = ChatBot(
+        api_secret_key,
+        model,
+        temperature,
+        max_tokens,
+        top_p,
+        frequency_penalty,
+        presence_penalty,
+    )
+
+    while True:
+        chatbot.answer_directly()
---
+++ main.py
@@ -0,0 +1,63 @@
+import os
+from pprint import pprint
+import yaml
+from slack import RTMClient
+from chatbot import ChatBot
+
+
+@RTMClient.run_on(event="message")
+def dont_say_any_cfdl_bot(**payload) -> None:
+    web_client = payload["web_client"]
+    data = payload["data"]
+
+    bot_id = data.get("bot_id", "")
+    subtype = data.get("subtype", "")
+    tag_code = data.get("text", "").split(" ")[0]
+
+    print("The message received is as follows.")
+    pprint(data)
+
+    if bot_id == "" and subtype == "" and ">" in tag_code:
+        channel_id = data["channel"]
+        text = data.get("text", "").split(">")[-1].strip()
+        message_ts = data["ts"]
+
+        response = chatbot.create(text)
+        web_client.chat_postMessage(
+            channel=channel_id, text=response, thread_ts=message_ts
+        )
+
+
+if __name__ == "__main__":
+    config_path = os.path.join("config.yaml")
+
+    with open(config_path, encoding="utf-8") as file:
+        config = yaml.safe_load(file)
+
+    api_secret_key = config["credentials"]["openai_api_secret_key"]
+    bot_user_oauth_token = config["credentials"]["slack_bot_user_oauth_token"]
+
+    model = config["chatbot"]["model"]
+    temperature = config["chatbot"]["temperature"]
+    max_tokens = config["chatbot"]["max_tokens"]
+    top_p = config["chatbot"]["top_p"]
+    frequency_penalty = config["chatbot"]["frequency_penalty"]
+    presence_penalty = config["chatbot"]["presence_penalty"]
+
+    chatbot = ChatBot(
+        api_secret_key,
+        model,
+        temperature,
+        max_tokens,
+        top_p,
+        frequency_penalty,
+        presence_penalty,
+    )
+
+    try:
+        rtm_client = RTMClient(token=bot_user_oauth_token)
+        print("Your bot is connected and running!")
+        rtm_client.start()
+
+    except Exception as error:
+        print(f"An error occurred and the message was: {error}")
